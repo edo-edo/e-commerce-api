7eba4a40a470c534284b06e0716a9c94391542e8
edo-edo <ediberidze99@gmail.com>
2021-12-03 23:36:38 +0400

    Remove express and switch to nest

--- src/api/modules/products/product.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { getProducts } from './product.controller';
-
-const route = Router();
-
-route.get('/', getProducts);
-
-export default route;
--- src/api/server.ts
+++
@@ -1,6 +0,0 @@
-import signale from 'signale';
-
-import app from './app';
-import '../database';
-
-app.listen(5000, () => signale.success(`Server listens to port 5000`));
